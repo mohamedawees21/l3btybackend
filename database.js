a8c6e10f3fab6bca47e14ee9d16ec42e5a646066
Mohamed <mohamedawees2121@gmail.com>
2026-02-28 03:51:48 +0200

    fix supabase ssl

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false
   }
